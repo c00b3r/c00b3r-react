cb7631fbc459294d047068cf1fd468ea6d189e24
c00b3r <sanuyOKmaklaren0>
2024-08-12 03:13:48 +0500

    chore: add style and finx switch dark light theme

--- my-next-app/src/context/LightDarkThemeContext.tsx
+++ my-next-app/src/context/LightDarkThemeContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 
 interface ThemeContextType {
   theme: string;
@@ -9,20 +9,26 @@ const DarkLightThemeContext = createContext<ThemeContextType | undefined>(
   undefined,
 );
 
-export const ThemeProvider: React.FC<{ children: ReactNode }> = ({
-  children,
-}) => {
-  const [theme, setTheme] = useState("light");
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export function ThemeProvider({ children }: ThemeProviderProps) {
+  const [theme, setTheme] = useState("dark");
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
   };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
 
   return (
     <DarkLightThemeContext.Provider value={{ theme, toggleTheme }}>
       {children}
     </DarkLightThemeContext.Provider>
   );
-};
+}
 
 export default DarkLightThemeContext;
