4c17dbad69689c8de969f106adcc280d1b0abc3d
c00b3r <sanuyOKmaklaren0>
2024-08-18 21:24:24 +0500

    feat: implement controlled form with React Hook Forms

--- src/pages/ControlledForm/ControlledForm.tsx
+++ src/pages/ControlledForm/ControlledForm.tsx
@@ -1,3 +1,66 @@
+import { Controller, useForm } from "react-hook-form";
+import "./ControlledForm.css";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+
 export default function ControlledForm() {
-  return <div>ControlledForm</div>;
+  const { register, control } = useForm();
+  const countries = useSelector((state: RootState) => state.country);
+  return (
+    <form>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input id="name" {...register("name")} />
+      </div>
+      <div>
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number" {...register("age")} />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input id="email" {...register("email")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="pasword" type="password" {...register("password")} />
+      </div>
+      <div>
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          id="confirmPassword"
+          type="password"
+          {...register("confirmPassword")}
+        />
+      </div>
+      <div>
+        <label htmlFor="gender">Gender</label>
+        <select id="gender" {...register("gender")}>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="accept">Acceptm Terms</label>
+        <input id="accept" type="checkbox" {...register("accept")} />
+      </div>
+      <div>
+        <label htmlFor="picture">Upload Picture</label>
+        <input id="picture" type="file" {...register("picture")} />
+      </div>
+      <div>
+        <label htmlFor="country">Country</label>
+        <Controller
+          name="country"
+          control={control}
+          render={({ field }) => <input list="country-list" {...field} />}
+        />
+      </div>
+      <datalist id="country-list">
+        {countries.map((country) => (
+          <option key={country} value={country} />
+        ))}
+      </datalist>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
